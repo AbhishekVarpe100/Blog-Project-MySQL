8b0aa775f48f1643c4614c1cfc7a878884f2a69f
Abhishek Varpe <101938194+AbhishekVarpe100@users.noreply.github.com>
2024-03-02 08:17:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static('public'))
+app.use(express.static('Public'))
 
 const sqlconnection = mysql.createConnection({
     host: '127.0.0.1',
